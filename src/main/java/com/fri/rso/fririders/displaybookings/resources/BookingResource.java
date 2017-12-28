e5667363a3d173d44a786967d4531237221fe672
Uros Bajc <ub6189@student.uni-lj.si>
2017-12-28 22:42:29 +0100

    implemented fault tolerance

--- src/main/java/com/fri/rso/fririders/displaybookings/resources/BookingResource.java
+++ src/main/java/com/fri/rso/fririders/displaybookings/resources/BookingResource.java
@@ -6,6 +6,7 @@ import com.fri.rso.fririders.displaybookings.entities.Accommodation;
 import com.fri.rso.fririders.displaybookings.entities.Booking;
 import com.fri.rso.fririders.displaybookings.database.Database;
 import com.fri.rso.fririders.displaybookings.entities.User;
+import com.fri.rso.fririders.displaybookings.services.BookingsBean;
 import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import com.kumuluz.ee.logs.LogManager;
 import com.kumuluz.ee.logs.Logger;
@@ -42,19 +43,14 @@ public class BookingResource {
     private ConfigProperties configProperties;
 
     @Inject
-    @DiscoverService(value = "accommodations", version = "1.0.x", environment = "dev")
-    private Optional<String> accommodationsUrl;
-
-    @Inject
-    @DiscoverService(value="users", version = "1.0.x", environment = "dev")
-    private Optional<String> usersUrl;
+    private BookingsBean bookingsBean;
 
 
     @GET
     @Metered
     public Response getAllBookings() {
         logger.info("REST CALL: getAllBookings");
-        List<Booking> bookings = Database.getBookings();
+        List<Booking> bookings = bookingsBean.getAllBookings();
         if(bookings != null && bookings.size() > 0) {
             logger.info("Found "+ bookings.size() + " bookings.");
             return Response.ok(bookings).build();
@@ -69,7 +65,7 @@ public class BookingResource {
     @Path("/{bookingId}")
     public Response getBooking(@PathParam("bookingId") int bookingId) {
         logger.info("REST CALL: getBooking.");
-        Booking booking = Database.getBooking(bookingId);
+        Booking booking = bookingsBean.getBooking(bookingId);
         if(booking != null) {
             return Response.ok(booking).build();
         }
@@ -81,56 +77,35 @@ public class BookingResource {
     @Path("/{bookingId}/accommodation")
     public Response getBookingAccommodation(@PathParam("bookingId") int bookingId) {
         logger.info("REST CALL: getBookingAccommodation.");
-        if (!this.accommodationsUrl.isPresent())
-            return Response.status(Response.Status.NOT_FOUND).entity("Accommodations service cannot be reached.").build();
-
-        Booking booking = Database.getBooking(bookingId);
-        if(booking != null) {
-            logger.info("Calling accommodations service.");
-            String url = this.accommodationsUrl.get() + "/accommodations/all";
-            logger.info("URL: " + url);
-
-            //find info about accommodation
-            List<Accommodation> accommodations =
-                    client.target(url)
-                            .request(MediaType.APPLICATION_JSON)
-                            .get((new GenericType<List<Accommodation>>() {}));
-            for(Accommodation a : accommodations)
-                if(a.getId() == booking.getId())
-                    return Response.ok(a).build();
+        try {
+            Accommodation accommodation = bookingsBean.getBookingAccommodation(bookingId);
+            if (accommodation != null)
+                return Response.ok(accommodation).build();
+            else {
+                logger.error("Accommodation for requested booking not found.");
+                return Response.status(Response.Status.NOT_FOUND).entity("Accommodation for requested booking not found.").build();
+            }
+        }
+        catch (Exception e) {
             return Response.status(Response.Status.NOT_FOUND).entity("Accommodation for requested booking not found.").build();
         }
-        else
-            return Response.status(Response.Status.NOT_FOUND).entity("Booking with id " + bookingId + " not found.").build();
     }
 
     @GET
     @Path("/{bookingId}/user")
     public Response getBookingUser(@PathParam("bookingId") int bookingId) {
         logger.info("REST CALL: getBookingUser.");
-        if (!this.usersUrl.isPresent())
-            return Response.status(Response.Status.NOT_FOUND).entity("Users service cannot be reached.").build();
-
-        Booking booking = Database.getBooking(bookingId);
-        if(booking != null) {
-            String userId = booking.getIdUser();
-            logger.info("Calling user service ...");
-            String url = this.usersUrl.get() + "/v1/users/" + userId;
-            logger.info("URL: " + url);
-
-            //find info about user
-            User user =
-                    client.target(url)
-                            .request(MediaType.APPLICATION_JSON)
-                            .get((new GenericType<User>() {}));
+        try {
+            User user = bookingsBean.getBookingUser(bookingId);
             if (user != null) {
-                logger.info("User with id" + userId+" successfully retrieved ...");
+                logger.info("User with id" + user.getUuid() + " successfully retrieved ...");
                 return Response.ok(user).build();
-            }
+            } else
+                return Response.status(Response.Status.NOT_FOUND).entity("User of requested booking not found.").build();
+        }
+        catch (Exception e) {
             return Response.status(Response.Status.NOT_FOUND).entity("User of requested booking not found.").build();
         }
-        else
-            return Response.status(Response.Status.NOT_FOUND).entity("Booking with id " + bookingId + " not found.").build();
     }
 
     @POST
@@ -142,27 +117,22 @@ public class BookingResource {
             return Response.status(Response.Status.FORBIDDEN).entity("Booking insertion is disabled.").build();
         }
 
-        try {
-            Database.addBooking(b);
+        boolean res = bookingsBean.addBooking(b);
+        if(res)
             return Response.ok(b).build();
-        }
-        catch (Exception e){
-            logger.error(e.getMessage());
-            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
-        }
+        else
+            return Response.status(Response.Status.CONFLICT).entity("Inserting booking in database failed.").build();
     }
 
     @DELETE
     @Path("/{bookingId}")
     public Response deleteBooking(@PathParam("bookingId") int bookingId) {
         logger.info("REST CALL: deleteBooking.");
-        try {
-            Database.deleteBooking(bookingId);
+
+        boolean res = bookingsBean.deleteBooking(bookingId);
+        if (res)
             return Response.ok("Booking successfully deleted.").build();
-        }
-        catch (Exception e){
-            logger.error(e.getMessage());
-            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
-        }
+        else
+            return Response.status(Response.Status.CONFLICT).entity("Deleting booking from database failed.").build();
     }
 }
---
+++ src/main/java/com/fri/rso/fririders/displaybookings/services/BookingsBean.java
@@ -0,0 +1,180 @@
+package com.fri.rso.fririders.displaybookings.services;
+
+import com.fri.rso.fririders.displaybookings.config.ConfigProperties;
+import com.fri.rso.fririders.displaybookings.database.Database;
+import com.fri.rso.fririders.displaybookings.entities.Accommodation;
+import com.fri.rso.fririders.displaybookings.entities.Booking;
+import com.fri.rso.fririders.displaybookings.entities.User;
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequestScoped
+public class BookingsBean {
+
+    private Logger logger = LogManager.getLogger(BookingsBean.class.getName());
+
+    private static Client client = ClientBuilder.newClient();
+
+    @Inject
+    private ConfigProperties configProperties;
+
+    @Inject
+    @DiscoverService(value = "accommodations", version = "1.0.x", environment = "dev")
+    private Optional<String> accommodationsUrl;
+
+    @Inject
+    @DiscoverService(value="users", version = "1.0.x", environment = "dev")
+    private Optional<String> usersUrl;
+
+
+    public List<Booking> getAllBookings() {
+        List<Booking> bookings = Database.getBookings();
+        if(bookings != null && bookings.size() > 0) {
+            logger.info("Found "+ bookings.size() + " bookings.");
+            return bookings;
+        }
+        else {
+            logger.error("Zero bookings found");
+            return new ArrayList<>();
+        }
+    }
+
+
+    public Booking getBooking(int bookingId) {
+        Booking booking = Database.getBooking(bookingId);
+        return booking;
+    }
+
+    @CircuitBreaker(requestVolumeThreshold = 2)
+    @Fallback(fallbackMethod = "getBookingsAccommodationFallback")
+    @Timeout(value = 5, unit = ChronoUnit.SECONDS)
+    public Accommodation getBookingAccommodation(int bookingId) {
+        if (this.accommodationsUrl.isPresent()){
+            Booking booking = Database.getBooking(bookingId);
+            if(booking != null) {
+                try {
+                    logger.info("Calling accommodations service.");
+                    String url = this.accommodationsUrl.get() + "/accommodations/all";
+                    logger.info("URL: " + url);
+
+                    //find info about accommodation
+                    List<Accommodation> accommodations =
+                            client.target(url)
+                                    .request(MediaType.APPLICATION_JSON)
+                                    .get((new GenericType<List<Accommodation>>() {
+                                    }));
+                    for (Accommodation a : accommodations)
+                        if (a.getId() == booking.getId())
+                            return a;
+                    return null;
+                }
+                catch (WebApplicationException | ProcessingException e) {
+                    logger.error(e);
+                    throw new InternalServerErrorException(e);
+                }
+            }
+            else
+                return null;
+        }
+        else{
+            logger.error("Accommodations service not reachable.");
+            return null;
+        }
+    }
+
+    public Object getBookingsAccommodationFallback(int bookingId) {
+        logger.warn("getBookingsAccommodationFallback called.");
+        return null;
+    }
+
+
+    @CircuitBreaker(requestVolumeThreshold = 2)
+    @Fallback(fallbackMethod = "getBookingUserFallback")
+    @Timeout(value = 5, unit = ChronoUnit.SECONDS)
+    public User getBookingUser(int bookingId) {
+        if (!this.usersUrl.isPresent()){
+            Booking booking = Database.getBooking(bookingId);
+            if(booking != null) {
+                try {
+                    String userId = booking.getIdUser();
+                    logger.info("Calling user service ...");
+                    String url = this.usersUrl.get() + "/v1/users/" + userId;
+                    logger.info("URL: " + url);
+
+                    //find info about user
+                    User user =
+                            client.target(url)
+                                    .request(MediaType.APPLICATION_JSON)
+                                    .get((new GenericType<User>() {
+                                    }));
+                    if (user != null) {
+                        logger.info("User with id" + userId + " successfully retrieved ...");
+                        return user;
+                    }
+                    return null;
+                }
+                catch (WebApplicationException | ProcessingException e) {
+                    logger.error(e);
+                    throw new InternalServerErrorException(e);
+                }
+            }
+            else
+                return null;
+        }
+        else{
+            logger.error("Users service not reachable.");
+            return null;
+        }
+    }
+
+    public Object getBookingUserFallback(int bookingId) {
+        logger.warn("getBookingUserFallback called.");
+        return null;
+    }
+
+    public boolean addBooking(Booking b){
+        // for etcd testing
+        if(!configProperties.isInsertEnabled()){
+            logger.error("Booking insertion is disabled.");
+            return false;
+        }
+
+        try {
+            Database.addBooking(b);
+            return true;
+        }
+        catch (Exception e){
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteBooking(int bookingId) {
+        try {
+            Database.deleteBooking(bookingId);
+            return true;
+        }
+        catch (Exception e){
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+}
